a8e980bd813ce379b36d128f1aa3e837f6f8d4bb
Xanthorrhizol <xanthorrhizol@proton.me>
2025-12-18 08:37:26 +0900

    B: Should use cp node's real ip to bootstrap nodes

--- 05_bootstrap.sh
+++ 05_bootstrap.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 function usage() {
-  echo "Usage: $0 <generated talosconfig path> <virtual ip(vip)> <first controlplane ip>"
+  echo "Usage: $0 <generated talosconfig path> <first controlplane ip>"
 }
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 2 ]; then
+  usage
+  exit 1
+fi
+
+if [ ! -f "$1" ]; then
+  echo "Talosconfig file not found: $1"
   usage
   exit 1
 fi
 
 TALOSCONFIG_PATH=$1
-VIP=$2
-FIRST_CONTROL_PLANE_IP=$3
+FIRST_CONTROL_PLANE_IP=$2
 
-talosctl bootstrap --talosconfig $TALOSCONFIG_PATH --endpoints $VIP --nodes $FIRST_CONTROL_PLANE_IP
+talosctl bootstrap --talosconfig $TALOSCONFIG_PATH --endpoints $FIRST_CONTROL_PLANE_IP --nodes $FIRST_CONTROL_PLANE_IP
