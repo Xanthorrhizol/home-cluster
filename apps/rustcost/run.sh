ce48b598913456fd2d40a2c0b3d3cb533c7b6677
Xanthorrhizol <xanthorrhizol@proton.me>
2025-12-19 18:19:47 +0900

    B: rustcost helm install command was wrong

--- apps/rustcost/run.sh
+++ apps/rustcost/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ $(helm repo list | grep rustcost | wc -l) -gt 0 ]; then
+if [ $(helm repo list | grep rustcost | wc -l) -eq 0 ]; then
   helm repo add rustcost https://rustcost.github.io/rustcost-helmchart
 fi
 helm repo update
-if [ $(kubectl get ns rustcosta | grep rustcost | wc -l) -lt 1 ]; then
+if [ $(kubectl get ns rustcost | grep rustcost | wc -l) -lt 1 ]; then
   kubectl create ns rustcost
 fi
-helm install -n rustcost rustcost rustcost/rustcost
+helm install -n rustcost rustcost rustcost
