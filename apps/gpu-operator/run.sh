d8d588cc61ca99b86daa2a06678578b9c8f35fb8
Xanthorrhizol <xanthorrhizol@proton.me>
2025-12-28 17:57:27 +0900

    F: add gpu-operator

---
+++ apps/gpu-operator/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $(helm repo list | grep nvidia | wc -l) -eq 0 ]; then
+  helm repo add nvidia https://helm.ngc.nvidia.com/nvidia
+  kubectl create ns gpu-operator
+  kubectl label --overwrite ns gpu-operator pod-security.kubernetes.io/enforce=privileged
+fi
+helm repo update
+if [ $(helm list | grep gpu-operator | wc -l) -eq 0 ]; then
+  helm install -n gpu-operator gpu-operator nvidia/gpu-operator
+else
+  helm upgrade -n gpu-operator gpu-operator nvidia/gpu-operator
+fi
