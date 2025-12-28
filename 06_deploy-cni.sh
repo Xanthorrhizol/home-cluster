1c8c712a34a1b3636b8d3b29a73f7f319baa7ad1
Xanthorrhizol <xanthorrhizol@proton.me>
2025-12-28 14:58:50 +0900

    B: cni-plugins package missed

--- 06_deploy-cni.sh
+++ 06_deploy-cni.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 cd $(dirname "$(readlink -f "$0")")
+
+source ./env
+NODES=(${CONTROLPLANE_NODES[@]} ${WORKER_NODES[@]})
+for NODE in ${NODES[@]}; do
+  ssh $NODE -C $" \
+    apk add cni-plugins && \
+    if [ ! -d /opt/cni/bin ]; then mkdir -p /opt/cni/bin; fi && \
+    ln -s /usr/libexec/cni/* /opt/cni/bin/"
+done
+
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
