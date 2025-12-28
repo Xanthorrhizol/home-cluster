2193280747f3a8f8bc0019d9f15352ed08c95885
Xanthorrhizol <xanthorrhizol@proton.me>
2025-12-28 12:40:58 +0900

    F: change cni into flannel

--- 06_deploy-cni.sh
+++ 06_deploy-cni.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 cd $(dirname "$(readlink -f "$0")")
-source ./env
-
-NODES=(${CONTROLPLANE_NODES[@]} ${WORKER_NODES[@]})
-
-for NODE in ${NODES[@]}; do
-  sleep 10
-  ssh $NODE -C $" \
-    mount --make-rshared /run/cilium/cgroupv2 && \
-    mount --make-rshared /sys/fs/bpf && \
-    mount --make-rslave /run && \
-    rc-service containerd restart && \
-    rc-service kubelet restart"
-done
-
-cilium-cli install --kubeconfig kubeconfig
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
