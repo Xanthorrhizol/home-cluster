f81505dab26cb565aa7e71cfcc94d70b73f343b8
Xanthorrhizol <xanthorrhizol@proton.me>
2025-12-28 23:18:21 +0900

    F: HTTPRoute for rustcost

---
+++ apps/rustcost/create-http-route.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+cd $(dirname "$(readlink -f "$0")")
+source ../../env
+kubectl label ns rustcost gateway-access=true
+
+cat << EOF > http-route.yaml
+apiVersion: gateway.networking.k8s.io/v1
+kind: HTTPRoute
+metadata:
+  name: web-route
+  namespace: rustcost
+spec:
+  parentRefs:
+    - name: home-gateway
+      namespace: gateway-api
+  hostnames:
+    - cost.${DOMAIN}
+  rules:
+    - backendRefs:
+      - name: rustcost-core-svc
+        port: 80
+      matches:
+        - path:
+            type: PathPrefix
+            value: /api/
+      filters:
+        - type: URLRewrite
+          urlRewrite:
+            path:
+              type: ReplacePrefixMatch
+              replacePrefixMatch: /
+    - backendRefs:
+      - name: rustcost-gpu-exporter
+        port: 8000
+      matches:
+        - path:
+            type: PathPrefix
+            value: /metrics/
+    - backendRefs:
+      - name: rustcost-dashboard-svc
+        port: 80
+      matches:
+        - path:
+            type: PathPrefix
+            value: /
+EOF
