3e09b7670a73ec17db425881ec03c9b7c8a4c9c9
Xanthorrhizol <xanthorrhizol@proton.me>
2025-12-28 23:17:15 +0900

    F: Envoy Gateway API added

--- apps/gateway-api/install.sh
+++ apps/gateway-api/install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/standard-install.yaml
+kubectl apply -f https://github.com/envoyproxy/gateway/releases/download/v1.6.1/install.yaml
