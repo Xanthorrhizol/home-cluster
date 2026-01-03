9feb4c2d7477489e2fd9da0b960063909df8b60f
Xanthorrhizol <xanthorrhizol@proton.me>
2026-01-03 13:52:55 +0900

    B: handle long config yaml from server-side

--- apps/gateway-api/install.sh
+++ apps/gateway-api/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd $(dirname "$(readlink -f "$0")")
 kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/standard-install.yaml
-kubectl apply -f https://github.com/envoyproxy/gateway/releases/download/v1.6.1/install.yaml
+kubectl apply --server-side -f https://github.com/envoyproxy/gateway/releases/download/v1.6.1/install.yaml
