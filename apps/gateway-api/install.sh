3b89fda8a1f3ed8133ac81be1e835f415cc23f46
Xanthorrhizol <xanthorrhizol@proton.me>
2026-01-03 13:54:58 +0900

    F: install gatewayclass and gateway once

--- apps/gateway-api/install.sh
+++ apps/gateway-api/install.sh
@@ -2,3 +2,5 @@
 cd $(dirname "$(readlink -f "$0")")
 kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/standard-install.yaml
 kubectl apply --server-side -f https://github.com/envoyproxy/gateway/releases/download/v1.6.1/install.yaml
+kubectl apply -f gateway-class.yaml
+kubectl apply -f gateway.yaml
