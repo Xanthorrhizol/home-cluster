c3dd69ac94875fa5ac7922d39f349c08548ceac4
Xanthorrhizol <xanthorrhizol@proton.me>
2026-01-16 07:52:40 +0900

    B: typo

--- 04_cluster/07_setup-gpu.sh
+++ 04_cluster/07_setup-gpu.sh
@@ -3,7 +3,7 @@ cd $(dirname "$(readlink -f "$0")")
 source ../env
 
 kubectl label --overwrite node $GPU_NODE nvidia.com/gpu.present=true
-kubectl apply runtimeclass-nvidia.yaml
+kubectl apply -f runtimeclass-nvidia.yaml
 
 if [ $(helm repo list | grep nvdp | wc -l) -eq 0 ]; then
   helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
